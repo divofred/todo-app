fa9a6c0e949dc0dd6b689e2a550d413c06c77156
Fredrick Emmanuel <fredrickemmanuelc@gmail.com>
2024-02-07 10:35:42 +0100

    todo-app

---
+++ src/lib/client.tsx
@@ -0,0 +1,38 @@
+// src/lib/client.ts
+'use client';
+
+import { HttpLink, InMemoryCache, ApolloLink } from '@apollo/client';
+import {
+  NextSSRApolloClient,
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  SSRMultipartLink
+} from '@apollo/experimental-nextjs-app-support/ssr';
+const STRAPI_URL = process.env.STRAPI_URL || 'https://todo-api-fg.onrender.com';
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: `${STRAPI_URL}/graphql`
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === 'undefined'
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true
+            }),
+            httpLink
+          ])
+        : httpLink
+  });
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
